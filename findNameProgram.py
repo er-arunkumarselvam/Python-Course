c50d181da89c322e2a819690da53e3343968a383
er-arunkumarselvam <er.arunkumar_selvam@outlook.com>
2023-03-17 20:54:14 +0530

    Practice While Loop Programs

---
+++ findNameProgram.py
@@ -0,0 +1,10 @@
+name='Arunkumar'
+person=''
+while name!=person:
+    person=input("What is your name? ")
+else:
+    print("I got the name.")
+i=0
+while i<len(name):
+    print(name[i],end=' ')
+    i+=1
