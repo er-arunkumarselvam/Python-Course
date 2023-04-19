c95c41567f4e7aeec752706acf4c2035a2a6aa0f
er-arunkumarselvam <er.arunkumar_selvam@outlook.com>
2023-04-19 21:00:29 +0530

    Immutability Property

---
+++ immutability.py
@@ -0,0 +1,24 @@
+# Task-1
+house_no=10
+print(type(house_no))
+print(id(house_no))#Id
+house_no=12
+print(id(house_no)) #Id Changed
+print()
+# Task-2
+mark=75
+print(id(mark))
+mark=mark+10
+print(id(mark))
+print()
+# Task-3
+count=0
+print(id(count))
+count+=1
+print(id(count))
+print()
+# Task-4 - Strings
+captain="Dhoni"
+print(id(captain))
+captain="Kolhi"
+print(id(captain))
