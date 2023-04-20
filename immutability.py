8aaef4b4bcf21f0adce35873584662f0804e8282
er-arunkumarselvam <er.arunkumar_selvam@outlook.com>
2023-04-20 21:04:33 +0530

    Immutability Property

--- immutability.py
+++ immutability.py
@@ -1,4 +1,6 @@
 # Task-1
+from typing import List
+
 house_no=10
 print(type(house_no))
 print(id(house_no))#Id
@@ -22,3 +24,27 @@ captain="Dhoni"
 print(id(captain))
 captain="Kolhi"
 print(id(captain))
+print()
+# Task-5 - Immutable
+city1="kumbakonam"
+city2="kumbakonam"
+print (city1 is city2)
+print (city1==city2)
+print(id(city1))
+print(id(city2))
+print()
+# Task-6
+# name='Raja'
+# name[1]='o'
+# print(name) # Traceback- TypeError: 'str' object does not support item assignment
+# print()
+#Task-7 - change letters
+name='class'
+print('g'+name[1:])
+print()
+# List (Mutable) => Only value changed not change memory address
+list=['arun',503,7.54,True]
+print(id(list))
+list[0]='arunkumar'
+print(id(list))
+
