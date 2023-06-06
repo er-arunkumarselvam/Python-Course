57b6e79888cee15dfb84de261becbfd451cfce1a
Arunkumar <107843046+er-arunkumar@users.noreply.github.com>
2023-06-06 18:44:17 +0530

    set Datatype

--- set.py
+++ set.py
@@ -14,3 +14,16 @@ print(details.pop()) # return value
 # Dictionary
 values = {}
 print(type(values)) # <class 'dict'>
+# Using Operators
+name1 = {"Arun", "Kumar", "Selvam"}
+name2 = {"Prasanna", "Praveen", "Selvam"}
+print(name1 - name2) # Only First Set values
+print(name1 | name2) # Either or set1 or set2 (Union)
+print(name1 & name2) # Intersection value only
+print(name1 ^ name2) # Only one value
+#program
+details = {"Arun", 24, True, 5.11}
+for values in details:
+    if isinstance(values, int):
+   # if type(i) is int:
+        print(values)
