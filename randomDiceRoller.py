96a2ae798967858408c1eb0c8a3c08860ecf88a4
Arunkumar <107843046+er-arunkumar@users.noreply.github.com>
2023-06-07 20:23:52 +0530

    Random Dice Roller Game

---
+++ randomDiceRoller.py
@@ -0,0 +1,21 @@
+# Random Dice Roller Game
+# Module - group of functions
+# Library - number of modules
+# from, import - python reserved keyword
+# random - Module
+# randint - function (2 argument passing) - integer - random integer
+
+from random import randint
+# from random (import only a module)
+minValue = 1
+maxValue = 6
+print("Random Dice Roller Game")
+playAgain = True
+while playAgain:
+    print("Dice Value now : ", randint(minValue, maxValue))  # random.randint(minValue, maxValue) using dot '.' split the module and functions
+    nextTime = input("Press 'y' if you want to roll on dice again : ")
+    if nextTime.lower() == 'y' or nextTime.lower() == 'yes':
+        playAgain = True
+    else:
+        playAgain = False
+        print("Thank you, Visit Again.")
